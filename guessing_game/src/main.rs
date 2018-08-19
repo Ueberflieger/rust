adfc68951f9225ec94d91dd67fbd9a01e7510b06
ueberflieger <github6776@gmail.com>
2018-08-20 07:54:50 +1000

    add input error checking see chapter 9.3

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,6 +4,26 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -22,9 +42,11 @@ fn main() {
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
+        let mut guess = Guess::new(guess);
 
-        match guess.cmp(&secret_number) {
+        println!("You guessed: {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
@@ -34,3 +56,4 @@ fn main() {
         }
     }
 }
+
