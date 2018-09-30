31029f2b8fe2713bd5d399bdc7f823f7fda3da08
ueberflieger <github6776@gmail.com>
2018-10-01 09:50:07 +1000

    Introduced class for parsing arguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,12 +5,12 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = Config::new(&args);
 
-    println!("Search {}", query);
-    println!("In file {}", filename);
+    println!("Search {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)
@@ -20,9 +20,16 @@ fn main() {
 
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename : String,
+}
 
-    (query, filename)
+impl Config{
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
 }
